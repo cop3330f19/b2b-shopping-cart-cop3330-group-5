0718825b026ffb5d1e45705235ea8a4e5c626abb
iyeshag <59750813+iyeshag@users.noreply.github.com>
2020-02-27 23:54:25 -0500

    Update customer.h

--- customer.h
+++ customer.h
@@ -1,46 +1,37 @@
-#include<vector>
 #include<string>
-#include "Address.h"
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
 
 class Customer
 {
     std::string customerNum;
     std::string customerName;
     double lineOfCredit;
-    Address * corperateAddress;
-}
+    std:: string * corperateAddress;
+    
+    public:
+    //Constructors for Customer Class
+    ////Object created for storing in the products class
+    
+    Customer(std::string, std::string, double);
+    Customer();
 
-public:
-//Constructors for Customer Class
-//Object created for storing in the products class
-Customer(std::string, std:string, double)
+   
+    //Get Functions
+   //Return customer number
+   std::string getCustomerNum();
+   
+   //Get Customer Name
+   std::string getCustomerName();
 
-Customer();
-
-//Vector
-
-std::vector<Customer>cust;
-std::vector<Address>addr;
-
-
-//Get Functions
-
-//Return cust num
-std::string getcustomerNum();
-
-//Get Customer Name
-std::string getcustomerName();
-
-//Get credit
-double getlineOfCredit();
-
-//Receive customer info for grabbing customer file for orders
-void grabcustomer();
-std::string getCustomer();
-
-//Set Functions
-void setcustomerNum(std::string);
-void setcustomerName(std::string);
-void setlineOfCredit(double);
-
-
+   //Get credit
+  double getLineOfCredit();
+  
+  //Set Functions
+  void setCustomerNum(std::string);
+  void setCustomerName(std::string);
+  void setLineOfCredit(double);
+  
+  
+};
+#endif
